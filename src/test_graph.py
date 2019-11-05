79ee6b60861bb2bf711c98f32428cc4b46f7f550
Pau Rodriguez <rodriguez.pau@gmail.com>
2019-11-05 17:09:15 +0100

    uniitest in place ready

---
+++ src/test_graph.py
@@ -0,0 +1,41 @@
+import unittest
+
+
+class TestGraph(unittest.TestCase):
+
+    def test_depth(self):
+        """ tests that graph generation uses correctly the depth parameter
+        """
+        self.assertTrue(False)
+
+    def test_breadth(self):
+        """ tests that graph generation uses correctly the breadth parameter
+        """
+        self.assertTrue(False)
+
+    def test_unique_entries(self):
+        """ 
+        """
+        self.assertTrue(False)
+
+
+    def test_recursion_in_loop_form(self):
+        """ 
+        """
+        self.assertTrue(True)
+
+
+    def test_rate_limiting(self):
+        """ 
+        """
+        self.assertTrue(True)
+
+
+    def test_extracted_graph(self):
+        """ 
+            use mock intensively here
+        """
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
